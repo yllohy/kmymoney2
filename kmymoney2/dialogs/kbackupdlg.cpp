558eb9d3b5a0b745553269811b86ab5e243269aa
Felix Rodriguez <frodriguez@users.sourceforge.net>
2001-06-07 23:33:12 +0000

    Adding backup dialogs file

---
+++ kmymoney2/dialogs/kbackupdlg.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+                          kbackupdialog.cpp  -  description
+                             -------------------
+    begin                : Mon Jun 4 2001
+    copyright            : (C) 2001 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                             Javier Campos Morales <javi_c@ctv.es>
+                             Felix Rodriguez <frodriguez@mail.wesleyan.edu>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "kbackupdlg.h"
+
+KBackupDlg::KBackupDlg( QWidget* parent,  const char* name, bool modal):kbackupdlgdecl( parent,  name , true){
+
+}
+KBackupDlg::~KBackupDlg(){
+}
---
+++ kmymoney2/dialogs/kbackupdlg.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+                          kbackupdialog.h  -  description
+                             -------------------
+    begin                : Mon Jun 4 2001
+    copyright            : (C) 2001 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                             Javier Campos Morales <javi_c@ctv.es>
+                             Felix Rodriguez <frodriguez@mail.wesleyan.edu>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KBACKUPDIALOG_H
+#define KBACKUPDIALOG_H
+
+#include <qwidget.h>
+#include "kbackupdlgdecl.h"
+
+/**
+  *@author Michael Edwardes
+  */
+
+class KBackupDlg : public kbackupdlgdecl  {
+	Q_OBJECT
+public: 
+	KBackupDlg( QWidget* parent,  const char* name, bool modal);
+	~KBackupDlg();
+};
+
+#endif
