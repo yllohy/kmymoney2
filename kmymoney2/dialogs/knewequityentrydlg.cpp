675fa87746080a8dae4c8149ae3407303cfa8f84
Kevin Tambascio <ktambascio@users.sourceforge.net>
2002-01-30 04:30:34 +0000

    Added a dialog for entering information about a new stock or mutual fund.

---
+++ kmymoney2/dialogs/knewequityentrydlg.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+                          knewequityentrydlg.cpp  -  description
+                             -------------------
+    begin                : Tue Jan 29 2002
+    copyright            : (C) 2000-2002 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kstddirs.h>
+#include <qpixmap.h>
+
+// ----------------------------------------------------------------------------
+// QT Includes
+#include <qfile.h>
+#include <qtextstream.h>
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qprogressbar.h>
+#include <qlineedit.h>
+#include <qgroupbox.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+#include <kfiledialog.h>
+#include <kglobal.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <kconfig.h>
+
+#include "knewequityentrydlg.h"
+
+KNewEquityEntryDlg::KNewEquityEntryDlg(QWidget *parent, const char *name)
+	: kNewEquityEntryDecl(parent, name)
+{
+	connect(btnOK, SIGNAL(clicked()), this, SLOT(slotOKClicked()));
+}
+
+KNewEquityEntryDlg::~KNewEquityEntryDlg()
+{
+}
+/** No descriptions */
+void KNewEquityEntryDlg::onOKClicked()
+{
+}
---
+++ kmymoney2/dialogs/knewequityentrydlg.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+                          knewequityentrydlg.h  -  description
+                             -------------------
+    begin                : Tue Jan 29 2002
+    copyright            : (C) 2000-2002 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KNEWEQUITYENTRY_H
+#define KNEWEQUITYENTRY_H
+
+#include <qdialog.h>
+#include <klocale.h>
+
+#include "knewequityentrydecl.h"
+
+/**
+	*
+	* Dialog to allow user to enter all data for a stock or mutual fund investment type.
+  *
+  * @author Kevin Tambascio
+  *
+  */
+
+class KNewEquityEntryDlg : public kNewEquityEntryDecl
+{
+	Q_OBJECT
+
+public:
+	KNewEquityEntryDlg(QWidget *parent = NULL, const char *name = NULL);
+	~KNewEquityEntryDlg();
+
+protected slots:
+  void onOKClicked();
+		
+	
+};
+
+#endif
