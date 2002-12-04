1f4452106cc949db6907cbeba3448d6501e63fd4
Thomas Baumgart <ipwizard@users.sourceforge.net>
2002-12-04 15:09:46 +0000

    Initial checkin

---
+++ kmymoney2/views/kledgerviewasset.cpp
@@ -0,0 +1,63 @@
+/***************************************************************************
+                          kledgerviewasset.cpp  -  description
+                             -------------------
+    begin                : Mon Dec 2 2002
+    copyright            : (C) 2000-2002 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "kledgerviewasset.h"
+#include "../widgets/kmymoneytransactionform.h"
+#include "../widgets/kmymoneyregister.h"
+#include "../widgets/kmymoneyedit.h"
+#include "../widgets/kmymoneydateinput.h"
+#include "../widgets/kmymoneypayee.h"
+#include "../widgets/kmymoneycategory.h"
+#include "../widgets/kmymoneylineedit.h"
+#include "../widgets/kmymoneycombo.h"
+
+KLedgerViewAsset::KLedgerViewAsset(QWidget *parent, const char *name )
+  : KLedgerViewCheckings(parent,name)
+{
+	m_register->horizontalHeader()->setLabel(4, i18n("Decrease"));
+	m_register->horizontalHeader()->setLabel(5, i18n("Increase"));
+
+  m_form->tabBar()->removeTab(m_tabCheck);
+  m_form->tabBar()->removeTab(m_tabAtm);
+  m_form->tabBar()->tabAt(0)->setText(i18n("Increase"));
+  m_form->tabBar()->tabAt(2)->setText(i18n("Decrease"));
+
+  m_register->repaintContents(false);
+}
+
+KLedgerViewAsset::~KLedgerViewAsset()
+{
+}
+
+void KLedgerViewAsset::slotReconciliation(void)
+{
+  KLedgerViewCheckings::slotReconciliation();
+}
---
+++ kmymoney2/views/kledgerviewcash.h
@@ -0,0 +1,58 @@
+/***************************************************************************
+                          kledgerviewcash.h  -  description
+                             -------------------
+    begin                : Mon Dec 2 2002
+    copyright            : (C) 2000-2002 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KLEDGERVIEWCASH_H
+#define KLEDGERVIEWCASH_H
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <qwidget.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "kledgerviewcheckings.h"
+
+/**
+  * @author Thomas Baumgart
+  */
+
+class KLedgerViewCash : public KLedgerViewCheckings
+{
+  Q_OBJECT
+
+public: 
+	KLedgerViewCash(QWidget *parent=0, const char *name=0);
+	~KLedgerViewCash();
+
+protected slots:
+
+  /* documented in base class */
+  virtual void slotReconciliation(void);
+
+};
+
+#endif
