51bff4e86d01223b65150ac57f67c60a58fd5d0b
Thomas Baumgart <ipwizard@users.sourceforge.net>
2003-09-19 19:13:34 +0000

    added new account selection widget

---
+++ kmymoney2/widgets/kmymoneyaccountselector.cpp
@@ -0,0 +1,412 @@
+/***************************************************************************
+                          kmymoneyaccountselector.cpp  -  description
+                             -------------------
+    begin                : Thu Sep 18 2003
+    copyright            : (C) 2003 by Thomas Baumgart
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <qlayout.h>
+#include <qheader.h>
+#include <qlabel.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+#include <klocale.h>
+#include <kpushbutton.h>
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "../kmymoneyutils.h"
+#include "../mymoney/mymoneyutils.h"
+#include "../mymoney/mymoneyfile.h"
+
+#include "kmymoneyaccountselector.h"
+
+kMyMoneyListViewItem::kMyMoneyListViewItem(QListView* parent, const QString& txt, const QCString& id) :
+  KListViewItem(parent, txt),
+  m_id(id)
+{
+}
+
+kMyMoneyListViewItem::kMyMoneyListViewItem(QListViewItem* parent, const QString& txt, const QCString& id) :
+  KListViewItem(parent, txt),
+  m_id(id)
+{
+}
+
+kMyMoneyListViewItem::~kMyMoneyListViewItem()
+{
+}
+
+kMyMoneyCheckListItem::kMyMoneyCheckListItem(QListView* parent, const QString& txt, const QCString& id, Type type) :
+  QCheckListItem(parent, txt, type),
+  m_id(id)
+{
+  setOn(true);
+}
+
+kMyMoneyCheckListItem::kMyMoneyCheckListItem(QListViewItem* parent, const QString& txt, const QCString& id, Type type) :
+  QCheckListItem(parent, txt, type),
+  m_id(id)
+{
+  setOn(true);
+}
+
+kMyMoneyCheckListItem::~kMyMoneyCheckListItem()
+{
+}
+
+void kMyMoneyCheckListItem::stateChange(bool state)
+{
+  emit stateChanged(state);
+}
+
+
+kMyMoneyAccountSelector::kMyMoneyAccountSelector(QWidget *parent, const char *name ) :
+  QWidget(parent, name)
+{
+  QHBoxLayout*   layout;
+  QVBoxLayout*   buttonLayout;
+  
+  m_selMode = QListView::Single;
+  
+  m_listView = new KListView(this);
+
+
+  layout = new QHBoxLayout( this, 0, 6, "accountSelectorLayout");
+
+  m_listView->addColumn( "Hidden" );
+  // m_listView->header()->setClickEnabled( FALSE, m_listView->header()->count() - 1 );
+  // m_listView->header()->setResizeEnabled( FALSE, m_listView->header()->count() - 1 );
+  m_listView->header()->hide();
+  
+  layout->addWidget( m_listView );
+
+  buttonLayout = new QVBoxLayout( 0, 0, 6, "accountSelectorButtonLayout");
+
+  m_allAccountsButton = new KPushButton( this, "m_allAccountsButton" );
+  m_allAccountsButton->setText( i18n( "All" ) );
+  buttonLayout->addWidget( m_allAccountsButton );
+
+  m_incomeCategoriesButton = new KPushButton( this, "m_incomeCategoriesButton" );
+  m_incomeCategoriesButton->setText( i18n( "Income" ) );
+  buttonLayout->addWidget( m_incomeCategoriesButton );
+
+  m_expenseCategoriesButton = new KPushButton( this, "m_expenseCategoriesButton" );
+  m_expenseCategoriesButton->setText( i18n( "Expense" ) );
+  buttonLayout->addWidget( m_expenseCategoriesButton );
+
+  m_noAccountButton = new KPushButton( this, "m_noAccountButton" );
+  m_noAccountButton->setText( i18n( "None" ) );
+  buttonLayout->addWidget( m_noAccountButton );
+  
+  QSpacerItem* spacer = new QSpacerItem( 0, 67, QSizePolicy::Minimum, QSizePolicy::Expanding );
+  buttonLayout->addItem( spacer );
+  layout->addLayout( buttonLayout );
+
+  // force init
+  m_selMode = QListView::Multi;
+  setSelectionMode(QListView::Single);
+  
+  connect(m_allAccountsButton, SIGNAL(clicked()), this, SLOT(slotSelectAllAccounts()));
+  connect(m_noAccountButton, SIGNAL(clicked()), this, SLOT(slotDeselectAllAccounts()));
+  connect(m_incomeCategoriesButton, SIGNAL(clicked()), this, SLOT(slotSelectIncomeCategories()));
+  connect(m_expenseCategoriesButton, SIGNAL(clicked()), this, SLOT(slotSelectExpenseCategories()));
+}
+
+kMyMoneyAccountSelector::~kMyMoneyAccountSelector()
+{
+}
+
+void kMyMoneyAccountSelector::setSelectionMode(const QListView::SelectionMode mode)
+{
+  if(m_selMode != mode) {
+    m_selMode = mode;
+    m_listView->clear();
+    
+    // make sure, it's either Multi or Single
+    if(mode != QListView::Multi) {
+      m_selMode = QListView::Single;
+
+      m_allAccountsButton->hide();
+      m_noAccountButton->hide();
+      m_incomeCategoriesButton->hide();
+      m_expenseCategoriesButton->hide();
+
+    } else {
+      m_allAccountsButton->show();
+      m_noAccountButton->show();
+      m_incomeCategoriesButton->show();
+      m_expenseCategoriesButton->show();
+    }
+  }
+  QWidget::update();
+}
+
+QListViewItem* kMyMoneyAccountSelector::newEntryFactory(QListViewItem* parent, const QString& name, const QCString& id)
+{
+  QListViewItem* p;
+  
+  if(m_selMode == QListView::Multi) {
+    kMyMoneyCheckListItem* q = new kMyMoneyCheckListItem(parent, name, id);
+    connect(q, SIGNAL(stateChanged(bool)), this, SIGNAL(stateChanged(void)));
+    p = static_cast<QListViewItem*> (q);
+    
+  } else {
+    kMyMoneyListViewItem* q = new kMyMoneyListViewItem(parent, name, id);
+    p = static_cast<QListViewItem*> (q);
+  }
+
+  return p;
+}
+
+void kMyMoneyAccountSelector::loadList(KMyMoneyUtils::categoryTypeE typeMask)
+{
+  QCStringList list;
+  QCStringList::ConstIterator it_l;
+  MyMoneyFile* file = MyMoneyFile::instance();
+
+  m_listView->clear();
+
+  if(m_selMode == QListView::Multi) {
+    m_incomeCategoriesButton->hide();
+    m_expenseCategoriesButton->hide();
+  }
+  
+  for(int mask = 0x01; mask != KMyMoneyUtils::last; mask <<= 1) {
+    kMyMoneyCheckListItem* item = 0;
+    if(typeMask & mask & KMyMoneyUtils::asset) {
+      item = new kMyMoneyCheckListItem(m_listView, i18n("Asset accounts"), QCString(), QCheckListItem::Controller);
+      list = file->asset().accountList();
+    }
+    
+    if(typeMask & mask & KMyMoneyUtils::liability) {
+      item = new kMyMoneyCheckListItem(m_listView, i18n("Liability accounts"), QCString(), QCheckListItem::Controller);
+      list = file->liability().accountList();
+    }
+    
+    if(typeMask & mask & KMyMoneyUtils::income) {
+      item = new kMyMoneyCheckListItem(m_listView, i18n("Income categories"), QCString(), QCheckListItem::Controller);
+      list = file->income().accountList();
+      if(m_selMode == QListView::Multi) {
+        m_incomeCategoriesButton->show();
+      }
+    }
+
+    if(typeMask & mask & KMyMoneyUtils::expense) {
+      item = new kMyMoneyCheckListItem(m_listView, i18n("Expense categories"), QCString(), QCheckListItem::Controller);
+      list = file->expense().accountList();
+      if(m_selMode == QListView::Multi) {
+        m_expenseCategoriesButton->show();
+      }
+    }
+  
+    if(item != 0) {
+      item->setSelectable(false);
+      item->setOpen(true);
+      // scan all matching accounts found in the engine
+      for(it_l = list.begin(); it_l != list.end(); ++it_l) {
+        MyMoneyAccount acc = file->account(*it_l);
+        QListViewItem* subItem = newEntryFactory(item, acc.name(), acc.id());
+        if(acc.accountList().count() > 0) {
+          subItem->setOpen(true);
+          loadSubAccounts(subItem, acc.accountList());
+        }
+      }
+    }
+  }
+}
+
+void kMyMoneyAccountSelector::loadSubAccounts(QListViewItem* parent, const QCStringList& list)
+{
+  QCStringList::ConstIterator it_l;
+  MyMoneyFile* file = MyMoneyFile::instance();
+
+  for(it_l = list.begin(); it_l != list.end(); ++it_l) {
+    MyMoneyAccount acc = file->account(*it_l);
+    QListViewItem* item = newEntryFactory(parent, acc.name(), acc.id());
+    if(acc.accountList().count() > 0) {
+      item->setOpen(true);
+      loadSubAccounts(item, acc.accountList());
+    }
+  }
+}
+
+const bool kMyMoneyAccountSelector::allAccountsSelected(void) const
+{
+  QListViewItem* it_v;
+
+  if(m_selMode == QListView::Single)
+    return false;
+    
+  for(it_v = m_listView->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(it_v->rtti() == 1) {
+      QCheckListItem* it_c = static_cast<QCheckListItem*>(it_v);
+      if(it_c->type() == QCheckListItem::CheckBox) {
+        if(!(it_c->isOn() && allAccountsSelected(it_v)))
+          return false;
+      } else {
+        if(!allAccountsSelected(it_v))
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+const bool kMyMoneyAccountSelector::allAccountsSelected(const QListViewItem *item) const
+{
+  QListViewItem* it_v;
+
+  for(it_v = item->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(it_v->rtti() == 1) {
+      QCheckListItem* it_c = static_cast<QCheckListItem*>(it_v);
+      if(!(it_c->isOn() && allAccountsSelected(it_v)))
+        return false;
+    }
+  }
+  return true;
+}
+
+
+void kMyMoneyAccountSelector::selectAllAccounts(const bool state)
+{
+  QListViewItem* it_v;
+
+  for(it_v = m_listView->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    QCheckListItem* it_c = static_cast<QCheckListItem*>(it_v);
+    if(it_c->type() == QCheckListItem::CheckBox) {
+      it_c->setOn(state);
+    }
+    selectAllSubAccounts(it_v, state);
+  }
+  emit stateChanged();
+}
+
+void kMyMoneyAccountSelector::selectAllSubAccounts(QListViewItem* item, const bool state)
+{
+  QListViewItem* it_v;
+
+  for(it_v = item->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    static_cast<QCheckListItem*>(it_v)->setOn(state);
+    selectAllSubAccounts(it_v, state);
+  }
+}
+
+void kMyMoneyAccountSelector::selectCategories(const bool income, const bool expense)
+{
+  QListViewItem* it_v;
+
+  for(it_v = m_listView->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(static_cast<QCheckListItem*>(it_v)->text() == i18n("Income categories"))
+      selectAllSubAccounts(it_v, income);
+    else
+      selectAllSubAccounts(it_v, expense);
+  }
+  emit stateChanged();
+}
+
+const QCStringList kMyMoneyAccountSelector::selectedAccounts(void) const
+{
+  QListViewItem*  it_v;
+  QCStringList    list;
+
+  if(m_selMode == QListView::Single) {
+    kMyMoneyListViewItem* it_c = static_cast<kMyMoneyListViewItem*>(m_listView->selectedItem());
+    if(it_c != 0)
+      list << it_c->accountId();
+    
+  } else {
+    for(it_v = m_listView->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+      if(it_v->rtti() == 1) {
+        kMyMoneyCheckListItem* it_c = static_cast<kMyMoneyCheckListItem*>(it_v);
+        if(it_c->type() == QCheckListItem::CheckBox) {
+          if(it_c->isOn())
+            list << (*it_c).accountId();
+        }
+        selectedAccounts(list, it_v);
+      }
+    }
+  }
+  return list;
+}
+
+void kMyMoneyAccountSelector::selectedAccounts(QCStringList& list, QListViewItem* item) const
+{
+  QListViewItem* it_v;
+
+  for(it_v = item->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(it_v->rtti() == 1) {
+      kMyMoneyCheckListItem* it_c = static_cast<kMyMoneyCheckListItem*>(it_v);
+      if(it_c->type() == QCheckListItem::CheckBox) {
+        if(it_c->isOn())
+          list << (*it_c).accountId();
+        selectedAccounts(list, it_v);
+      }
+    }
+  }
+}
+
+void kMyMoneyAccountSelector::setSelected(const QCString& id)
+{
+  QListViewItem* it_v;
+
+  for(it_v = m_listView->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(it_v->rtti() == 1) {
+      kMyMoneyCheckListItem* it_c = static_cast<kMyMoneyCheckListItem*>(it_v);
+      if(it_c->accountId() == id) {
+        m_listView->setSelected(it_v, true);
+        return;
+      }
+    } else if(it_v->rtti() == 0) {
+      kMyMoneyListViewItem* it_c = static_cast<kMyMoneyListViewItem*>(it_v);
+      if(it_c->accountId() == id) {
+        m_listView->setSelected(it_v, true);
+        return;
+      }
+    }
+    setSelected(it_v, id);
+  }
+}
+
+void kMyMoneyAccountSelector::setSelected(QListViewItem* item, const QCString& id)
+{
+  QListViewItem* it_v;
+  
+  for(it_v = item->firstChild(); it_v != 0; it_v = it_v->nextSibling()) {
+    if(it_v->rtti() == 1) {
+      kMyMoneyCheckListItem* it_c = static_cast<kMyMoneyCheckListItem*>(it_v);
+      if(it_c->accountId() == id) {
+        m_listView->setSelected(it_v, true);
+        return;
+      }
+    } else if(it_v->rtti() == 0) {
+      kMyMoneyListViewItem* it_c = static_cast<kMyMoneyListViewItem*>(it_v);
+      if(it_c->accountId() == id) {
+        m_listView->setSelected(it_v, true);
+        return;
+      }
+    }
+    setSelected(it_v, id);
+  }
+}
---
+++ kmymoney2/widgets/kmymoneyaccountselector.h
@@ -0,0 +1,259 @@
+/***************************************************************************
+                          kmymoneyaccountselector.h  -  description
+                             -------------------
+    begin                : Thu Sep 18 2003
+    copyright            : (C) 2003 by Thomas Baumgart
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KMYMONEYACCOUNTSELECTOR_H
+#define KMYMONEYACCOUNTSELECTOR_H
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <qwidget.h>
+#include <qlistview.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+#include <klistview.h>
+class KPushButton;
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include <../kmymoneyutils.h>
+
+/**
+  * @author Thomas Baumgart
+  */
+
+/**
+  * This class implements a derived version of a QCheckListItem that
+  * allows the storage of an account id with the object and emits
+  * a signal upon state change.
+  */
+class kMyMoneyCheckListItem : public QObject, public QCheckListItem
+{
+  Q_OBJECT
+public:
+  kMyMoneyCheckListItem(QListView *parent, const QString& txt, const QCString& id, Type type = QCheckListItem::CheckBox);
+  kMyMoneyCheckListItem(QListViewItem *parent, const QString& txt, const QCString& id, Type type = QCheckListItem::CheckBox);
+  ~kMyMoneyCheckListItem();
+  const QCString& accountId(void) const { return m_id; };
+
+signals:
+  void stateChanged(bool);
+  
+protected:
+  virtual void stateChange(bool);
+
+private:
+  QCString             m_id;
+};
+
+/**
+  * This class implements a derived version of a QListViewItem that
+  * allows the storage of an account id with the object
+  */
+class kMyMoneyListViewItem : public QObject, public KListViewItem
+{
+  Q_OBJECT
+public:
+  kMyMoneyListViewItem(QListView *parent, const QString& txt, const QCString& id);
+  kMyMoneyListViewItem(QListViewItem *parent, const QString& txt, const QCString& id);
+  ~kMyMoneyListViewItem();
+  const QCString& accountId(void) const { return m_id; };
+
+private:
+  QCString             m_id;
+};
+
+/**
+  * This class implements an account/category selector. It is based
+  * on a tree view. Using this widget, one can select one or multiple
+  * accounts depending on the mode of operation and the set of accounts
+  * selected to be displayed. (see setSelectionMode()
+  * and loadList() about the specifics of configuration).
+  *
+  * - Single selection mode\n
+  *   In this mode the widget allows to select a single entry out of
+  *   the set of displayed accounts.
+  *
+  * - Multi selection mode\n
+  *   In this mode, the widget allows to select one or more entries
+  *   out of the set of displayed accounts. Selection is performed
+  *   by marking the account in the view.
+  */
+class kMyMoneyAccountSelector : public QWidget
+{
+  Q_OBJECT
+public: 
+  kMyMoneyAccountSelector(QWidget *parent=0, const char *name=0);
+  ~kMyMoneyAccountSelector();
+
+  /**
+    * This method sets the mode of operation of this widget.
+    * Supported values are @p QListView::Single and @p QListView::Multi.
+    *
+    * @param mode @p QListView::Single selects single selection mode and
+    *             @p QListView::Multi selects multi selection mode
+    *
+    * @note This method must be called prior to loadList(). When the
+    *       widget is created, it defaults to QListView::Single.
+    *       
+    */
+  void setSelectionMode(const QListView::SelectionMode mode);
+
+  /**
+    * This method loads the set of accounts into the widget
+    * as defined by the parameter @p typeMask. @p typeMask is
+    * a bit mask. The following definitions can be passed:
+    *
+    * - KMyMoneyUtils::asset
+    * - KMyMoneyUtils::liability
+    * - KMyMoneyUtils::income
+    * - KMyMoneyUtils::expense
+    *
+    * If multiple sets should be displayed, the above values
+    * can be logically 'or'-ed.
+    *
+    * @param typeMask bitmask defining which types of accounts
+    *                 should be loaded into the widget
+    *
+    */
+  void loadList(KMyMoneyUtils::categoryTypeE typeMask);
+
+  /**
+    * This method returns the list of selected account id's. If
+    * no account is selected, the list is empty.
+    *
+    * @return list of selected accounts
+    */
+  const QCStringList selectedAccounts(void) const;
+
+  /**
+    * This method returns an information if all items
+    * currently shown are selected or not.
+    *
+    * @retval true All accounts shown are selected
+    * @retval false Not all accounts are selected
+    *
+    * @note If the selection mode is set to Single, this
+    *       method always returns false.
+    */
+  const bool allAccountsSelected(void) const;
+
+  /**
+    * This method sets the current selected account.
+    */
+  void setSelected(const QCString& id);
+
+public slots:
+  /**
+    * This slot selects all items that are currently in
+    * the account list of the widget.
+    */
+  void slotSelectAllAccounts(void) { selectAllAccounts(true); };
+
+  /**
+    * This slot deselects all items that are currently in
+    * the account list of the widget.
+    */
+  void slotDeselectAllAccounts(void) { selectAllAccounts(false); };
+
+signals:
+  void stateChanged(void);
+  
+protected:
+  /**
+    * Helper method for setSelected() to traverse the tree.
+    */
+  void setSelected(QListViewItem *item, const QCString& id);
+  
+  /**
+    * Helper method for selectedAccounts() to traverse the tree.
+    */
+  void selectedAccounts(QCStringList& list, QListViewItem* item) const;
+  
+  /**
+    * Helper method for allAccountsSelected() to traverse the tree.
+    */
+  const bool allAccountsSelected(const QListViewItem *item) const;
+  
+  /**
+    * This method creates a new selectable object depending on the
+    * selection mode. This is either a KListViewItem for single
+    * selection mode or a KMyMoneyCheckListItem for multi selection mode
+    *
+    * @return pointer to newly created object
+    */
+  QListViewItem* newEntryFactory(QListViewItem* parent, const QString& name, const QCString& id);
+
+  /**
+    * This method loads the list of subaccounts as found in the
+    * @p list and attaches them to the parent widget passed as @p parent.
+    *
+    * @param parent pointer to parent widget
+    * @param list QCStringList containing the ids of all subaccounts to load
+    */
+  void loadSubAccounts(QListViewItem* parent, const QCStringList& list);
+
+  /**
+    * This method selects/deselects all items that
+    * are currently in the account list according
+    * to the parameter @p state.
+    *
+    * @param state select items if @p true, deselect otherwise
+    */
+  void selectAllAccounts(const bool state);
+
+  /**
+    * This is a helper method for selectAllItems().
+    */
+  void selectAllSubAccounts(QListViewItem* item, const bool state);
+
+  /**
+    * This is a helper method for selectAllIncomeCategories()
+    * and selectAllExpenseCategories().
+    */
+  void selectCategories(const bool income, const bool expense);
+  
+protected slots:
+  /**
+    * This slot selects all income categories
+    */      
+  void slotSelectIncomeCategories(void) { selectCategories(true, false); };
+  
+  /**
+    * This slot selects all expense categories
+    */
+  void slotSelectExpenseCategories(void) { selectCategories(false, true); };
+  
+private:
+  QListView::SelectionMode  m_selMode;
+  KListView*                m_listView;
+  
+  KPushButton*              m_allAccountsButton;
+  KPushButton*              m_noAccountButton;
+  KPushButton*              m_incomeCategoriesButton;
+  KPushButton*              m_expenseCategoriesButton;
+};
+
+#endif
