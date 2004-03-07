698963b0245b88fe245e88054bdb8bc84393af9b
Kevin Tambascio <ktambascio@users.sourceforge.net>
2004-03-07 03:04:27 +0000

    New dialog to allow users to edit an Equity object.

---
+++ kmymoney2/dialogs/keditequityentrydlg.cpp
@@ -0,0 +1,63 @@
+/***************************************************************************
+                          keditequityentrydlg.cpp  -  description
+                             -------------------
+    begin                : Sat Mar 6 2004
+    copyright            : (C) 2000-2004 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <kglobal.h>
+#include <klocale.h>
+#if QT_VERSION > 300
+#include <kstandarddirs.h>
+#else
+#include <kstddirs.h>
+#endif
+
+#include <qpixmap.h>
+
+// ----------------------------------------------------------------------------
+// QT Includes
+#include <qfile.h>
+#include <qtextstream.h>
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qprogressbar.h>
+#include <qlineedit.h>
+#include <qgroupbox.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+#include <kfiledialog.h>
+#include <kglobal.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <kconfig.h>
+#include <knuminput.h>
+                          
+#include "keditequityentrydlg.h"
+
+KEditEquityEntryDlg::KEditEquityEntryDlg(QWidget *parent = NULL, const char *name = NULL)
+  : kEditEquityEntryDecl(parent, name, true)
+{
+  
+}
+
+KEditEquityEntryDlg::~KEditEquityEntryDlg()
+{
+  
+}
---
+++ kmymoney2/dialogs/keditequityentrydlg.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+                          keditequityentrydlg.h  -  description
+                             -------------------
+    begin                : Sat Mar 6 2004
+    copyright            : (C) 2000-2004 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#ifndef KEDITEQUITYENTRYDLG_H
+#define KEDITEQUITYENTRYDLG_H
+
+#include <qdialog.h>
+#include <klocale.h>
+
+#include "keditequityentrydecl.h"
+
+/**
+  *@author Kevin Tambascio
+  */
+
+class KEditEquityEntryDlg : public kEditEquityEntryDecl
+{
+  Q_OBJECT
+public: 
+	KEditEquityEntryDlg(QWidget *parent = NULL, const char *name = NULL);
+	~KEditEquityEntryDlg();
+};
+
+#endif
