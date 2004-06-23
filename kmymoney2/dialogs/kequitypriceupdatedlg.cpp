a0d640181dffaad39f7d5f291230680fecb1a118
Kevin Tambascio <ktambascio@users.sourceforge.net>
2004-06-23 11:01:51 +0000

    - added new dialog for updating the price of stocks and currencies online.  Dialog is currently blank, but it will serve as the dialog that uses the online price updater being worked on by Tony.

---
+++ kmymoney2/dialogs/kequitypriceupdatedlg.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+                          kenterscheduledialog.cpp  -  description
+                             -------------------
+    begin                : Mon Sep 1 2003
+    copyright            : (C) 2000-2003 by Michael Edwardes
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qradiobutton.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <kpushbutton.h>
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "kequitypriceupdatedlg.h"
+
+KEquityPriceUpdateDlg::KEquityPriceUpdateDlg(QWidget *parent) : KEquityPriceUpdateDlgDecl(parent)
+{
+
+}
+
+KEquityPriceUpdateDlg::~KEquityPriceUpdateDlg()
+{
+
+}
---
+++ kmymoney2/dialogs/kequitypriceupdatedlg.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+                          kequitypriceupdatedlg.h  -  description
+                             -------------------
+    begin                : Tuesday June 22nd, 2004
+    copyright            : (C) 2000-2004 by Kevin Tambascio
+    email                : mte@users.sourceforge.net
+                           Javier Campos Morales <javi_c@users.sourceforge.net>
+                           Felix Rodriguez <frodriguez@users.sourceforge.net>
+                           John C <thetacoturtle@users.sourceforge.net>
+                           Thomas Baumgart <ipwizard@users.sourceforge.net>
+                           Kevin Tambascio <ktambascio@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KEQUITYPRICEUPDATEDIALOG_H
+#define KEQUITYPRICEUPDATEDIALOG_H
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <qwidget.h>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "kequitypriceupdatedlgdecl.h"
+
+
+/**
+  * @author Kevin Tambascio
+  */
+
+class KEquityPriceUpdateDlg : public KEquityPriceUpdateDlgDecl
+{
+  Q_OBJECT
+public: 
+  KEquityPriceUpdateDlg(QWidget *parent);
+  ~KEquityPriceUpdateDlg();
+
+};
+
+#endif
