2f9cb1c5c7a0c2d9a20b6e7cbdeb5679e8944c84
Ace Jones <acejones@users.sourceforge.net>
2005-01-05 05:02:22 +0000

    * Hooked up multiple sources for online quotes   * Moved online quote logic out of KEquityPriceUpdateDlg and into the new     WebPriceQuote class.   * Moved online quote configuration logic out of kMyMoneyOnlineQuoteConfig     and into the new WebPriceQuote class.   * Fixed a test case in KReportsViewTest::testInvestment to use the correct     year (2004, not "current year").

---
+++ kmymoney2/converter/webpricequote.cpp
@@ -0,0 +1,409 @@
+/***************************************************************************
+                          webpricequote.cpp
+                             -------------------
+    begin                : Thu Dec 30 2004
+    copyright            : (C) 2004 by Ace Jones
+    email                : Ace Jones <acejones@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// ----------------------------------------------------------------------------
+// QT Headers
+
+#include <qfile.h>
+#include <qregexp.h>
+
+// ----------------------------------------------------------------------------
+// KDE Headers
+
+#include <kio/netaccess.h>
+#include <kurl.h>
+#include <klocale.h>
+#include <kdebug.h>
+#include <kapplication.h>
+#include <kconfig.h>
+#include <kglobal.h>
+
+// ----------------------------------------------------------------------------
+// Project Headers
+
+#include "webpricequote.h"
+
+WebPriceQuote::WebPriceQuote( QObject* _parent, const char* _name ):
+  QObject( _parent, _name )
+{
+  connect(&m_filter,SIGNAL(processExited(const QString&)),this,SLOT(slotParseQuote(const QString&)));
+}
+
+WebPriceQuote::~WebPriceQuote()
+{
+}
+  
+bool WebPriceQuote::launch( const QString& _symbol, const QString& _sourcename )
+{
+  bool result = true;
+  m_symbol = _symbol;
+
+  // if we're running normally, with a UI, we can just get these the normal way,
+  // from the config gile
+  if ( kapp )
+  {
+    if ( _sourcename.isEmpty() )
+      m_source = WebPriceQuoteSource("Yahoo");
+    else
+      m_source = WebPriceQuoteSource(_sourcename);
+  }
+  // otherwise, if we have no kapp, we have no config.  so we just get them from
+  // the defaults
+  else
+  {
+    if ( _sourcename.isEmpty() )
+      m_source = defaultQuoteSources()["Yahoo"];
+    else
+      m_source = defaultQuoteSources()[_sourcename];
+  }
+  
+  KURL url = KURL::fromPathOrURL(m_source.m_url.arg(m_symbol));
+
+  // If we're running a non-interactive session (with no UI), we can't
+  // use KIO::NetAccess, so we have to get our web data the old-fashioned
+  // way... with 'wget'.
+  //
+  // Note that a 'non-interactive' session right now means only the test
+  // cases.  Although in the future if KMM gains a non-UI mode, this would
+  // still be useful
+  if ( ! kapp && ! url.isLocalFile() )
+    url = KURL::fromPathOrURL("/usr/bin/wget -O - " + url.prettyURL());
+    
+  if ( url.isLocalFile() )
+  {
+    emit status(QString("Executing %1...").arg(url.path()));
+
+    m_filter.clearArguments();
+    m_filter << QStringList::split(" ",url.path());
+    m_filter.setSymbol(m_symbol);
+
+    // if we're running non-interactive, we'll need to block.
+    // otherwise, just let us know when it's done.
+    KProcess::RunMode mode = KProcess::NotifyOnExit;
+    if ( ! kapp )
+      mode = KProcess::Block;
+    
+    if(m_filter.start(mode, KProcess::All))
+    {
+      result = true;
+      m_filter.resume();
+    }
+    else
+    {
+      emit error(QString("Unable to launch: %1").arg(url.path()));
+      slotParseQuote(QString());
+    } 
+  }
+  else
+  {
+    emit status(QString("Fetching URL %1...").arg(url.prettyURL()));
+
+    QString tmpFile;
+    if( KIO::NetAccess::download( url, tmpFile, NULL ) )
+    {
+      kdDebug(2) << "Downloaded " << tmpFile << endl;
+      QFile f(tmpFile);
+      if ( f.open( IO_ReadOnly ) )
+      {
+        result = true;
+        QString quote = QTextStream(&f).read();
+        f.close();
+        slotParseQuote(quote);
+      }
+      else
+      {
+        slotParseQuote(QString());
+      }
+      KIO::NetAccess::removeTempFile( tmpFile );
+    }
+    else
+    {
+      emit error(KIO::NetAccess::lastErrorString());
+      slotParseQuote(QString());
+    }
+  }
+  return result;
+}
+
+#include <qfile.h>
+#include <qtextstream.h>
+
+void WebPriceQuote::slotParseQuote(const QString& _quotedata)
+{
+  QString quotedata = _quotedata;
+  bool gotprice = false;
+  bool gotdate = false;
+
+//    kdDebug(2) << "WebPriceQuote::slotParseQuote( " << _quotedata << " ) " << endl;
+  
+  if ( ! quotedata.isEmpty() )
+  {
+    //
+    // First, remove extranous non-data elements
+    //
+
+    // HTML tags
+    quotedata.remove(QRegExp("<[^>]*>"));
+
+    // &...;'s
+    quotedata.replace(QRegExp("&[^;]+;")," ");
+    
+    // Extra white space
+    quotedata = quotedata.simplifyWhiteSpace();      
+        
+    QFile file("stripped.txt");
+    if ( file.open( IO_WriteOnly ) )
+    {
+      QTextStream( &file ) << quotedata;
+      file.close();
+    }
+      
+    QRegExp symbolRegExp(m_source.m_sym);
+    QRegExp dateRegExp(m_source.m_date);
+    QRegExp priceRegExp(m_source.m_price);
+
+    if( symbolRegExp.search(quotedata) > -1)
+      emit status(i18n("Symbol found: %1").arg(symbolRegExp.cap(1)));
+
+    if(priceRegExp.search(quotedata)> -1)
+    {
+      gotprice = true;
+      m_price = MyMoneyMoney(priceRegExp.cap(1).toDouble()).toString();
+      emit status(i18n("Price found: %1").arg(m_price.toString()));
+    }
+
+    if(dateRegExp.search(quotedata) > -1)
+    {
+      QString datestr = dateRegExp.cap(1);
+      // TODO: Fix this temporary hack.  We know yahoo returns mm/dd/yyyy
+      QRegExp dateparse("([0-9]+)/([0-9]+)/([0-9]+)");
+      if ( dateparse.search( datestr ) > -1 )
+      {
+        gotdate = true;
+        m_date = QDate( dateparse.cap(3).toInt(), dateparse.cap(1).toInt(), dateparse.cap(2).toInt() );
+        emit status(i18n("Date found: %1").arg(m_date.toString()));;
+      }
+    }
+
+    if ( gotprice && gotdate )
+    {
+      emit quote( m_symbol, m_date, m_price );
+    }
+    else
+    {
+      emit error(i18n("Unable to update price for %1").arg(m_symbol));
+    }
+  }
+}
+
+QMap<QString,WebPriceQuoteSource> WebPriceQuote::defaultQuoteSources(void)
+{
+  QMap<QString,WebPriceQuoteSource> result;
+
+  result["Yahoo"] = WebPriceQuoteSource("Yahoo", 
+    "http://finance.yahoo.com/d/quotes.csv?s=%1&f=sl1d1",
+    "\"([^,\"]*)\",.*",  // symbolregexp
+    "[^,]*,([^,]*),.*", // priceregexp
+    "[^,]*,[^,]*,\"([^\"]*)\"" // dateregexp
+  );
+
+  result["Yahoo Currency"] = WebPriceQuoteSource("Yahoo Currency", 
+    "http://finance.yahoo.com/d/quotes.csv?s=%1%2=X&f=sl1d1",
+    "\"([^,\"]*)\",.*",  // symbolregexp
+    "[^,]*,([^,]*),.*", // priceregexp
+    "[^,]*,[^,]*,\"([^\"]*)\"" // dateregexp
+  );
+
+  result["Yahoo UK"] = WebPriceQuoteSource("Yahoo UK", 
+    "http://uk.finance.yahoo.com/d/quotes.csv?s=%1&f=sl1d1",
+    "\"([^,\"]*)\",.*",  // symbolregexp
+    "[^,]*,([^,]*),.*", // priceregexp
+    "[^,]*,[^,]*,\"([^\"]*)\"" // dateregexp
+  );
+  
+  return result;
+}
+
+QStringList WebPriceQuote::quoteSources(void)
+{
+  KConfig *kconfig = KGlobal::config();
+  QStringList groups = kconfig->groupList();
+
+  QStringList::Iterator it;
+  QRegExp onlineQuoteSource(QString("^Online-Quote-Source-(.*)$"));
+
+  // get rid of all 'non online quote source' entries
+  for(it = groups.begin(); it != groups.end(); it = groups.remove(it)) {
+    if(onlineQuoteSource.search(*it) >= 0) {
+      // Insert the name part
+      groups.insert(it, onlineQuoteSource.cap(1));
+    }
+  }
+
+  // if the user has the OLD quote source defined, now is the
+  // time to remove that entry and convert it to the new system.
+  if ( ! groups.count() )
+  {
+    kconfig->setGroup("Online Quotes Options");
+    QString url(kconfig->readEntry("URL","http://finance.yahoo.com/d/quotes.csv?s=%1&f=sl1d1"));
+    QString symbolRegExp(kconfig->readEntry("SymbolRegex","\"([^,\"]*)\",.*"));
+    QString priceRegExp(kconfig->readEntry("PriceRegex","[^,]*,([^,]*),.*"));
+    QString dateRegExp(kconfig->readEntry("DateRegex","[^,]*,[^,]*,\"([^\"]*)\""));
+    kconfig->deleteGroup("Online Quotes Options");
+    
+    groups += "Old Source";
+    kconfig->setGroup(QString("Online-Quote-Source-%1").arg("Old Source"));
+    kconfig->writeEntry("URL", url);
+    kconfig->writeEntry("SymbolRegex", symbolRegExp);
+    kconfig->writeEntry("PriceRegex",priceRegExp);
+    kconfig->writeEntry("DateRegex", dateRegExp);
+    kconfig->sync();
+  }
+
+  // Set up each of the default sources.  These are done piecemeal so that
+  // when we add a new source, it's automatically picked up.
+  QMap<QString,WebPriceQuoteSource> defaults = defaultQuoteSources();
+  QMap<QString,WebPriceQuoteSource>::const_iterator it_source = defaults.begin();
+  while ( it_source != defaults.end() )
+  {
+    if ( ! groups.contains( (*it_source).m_name ) )
+    {
+      groups += (*it_source).m_name;
+      (*it_source).write();
+      kconfig->sync();
+    }
+    ++it_source;
+  }
+    
+  return groups;
+}
+
+//
+// Helper class to load/save an individual source
+//
+
+WebPriceQuoteSource::WebPriceQuoteSource(const QString& name, const QString& url, const QString& sym, const QString& price, const QString& date):
+  m_name(name),
+  m_url(url),
+  m_sym(sym),
+  m_price(price),
+  m_date(date)
+{
+}
+
+WebPriceQuoteSource::WebPriceQuoteSource(const QString& name)
+{
+  m_name = name;
+  KConfig *kconfig = KGlobal::config();
+  kconfig->setGroup(QString("Online-Quote-Source-%1").arg(m_name));
+  m_sym = kconfig->readEntry("SymbolRegex");
+  m_date = kconfig->readEntry("DateRegex");
+  m_price = kconfig->readEntry("PriceRegex");
+  m_url = kconfig->readEntry("URL");
+}
+
+void WebPriceQuoteSource::write(void) const
+{
+  KConfig *kconfig = KGlobal::config();
+  kconfig->setGroup(QString("Online-Quote-Source-%1").arg(m_name));
+  kconfig->writeEntry("URL", m_url);
+  kconfig->writeEntry("PriceRegex", m_price);
+  kconfig->writeEntry("DateRegex", m_date);
+  kconfig->writeEntry("SymbolRegex", m_sym);
+}
+
+void WebPriceQuoteSource::rename(const QString& name)
+{
+  remove();
+  m_name = name;
+  write();
+}
+
+void WebPriceQuoteSource::remove(void) const
+{
+  KConfig *kconfig = KGlobal::config();
+  kconfig->deleteGroup(QString("Online-Quote-Source-%1").arg(m_name));
+}
+
+//
+// Helper class to babysit the KProcess used for running the local script in that case
+//
+
+WebPriceQuoteProcess::WebPriceQuoteProcess(void)
+{
+  connect(this, SIGNAL(receivedStdout(KProcess*, char*, int)), this, SLOT(slotReceivedDataFromFilter(KProcess*, char*, int)));
+  connect(this, SIGNAL(processExited(KProcess*)), this, SLOT(slotProcessExited(KProcess*)));
+}
+
+void WebPriceQuoteProcess::slotReceivedDataFromFilter(KProcess* /*_process*/, char* _pcbuffer, int _nbufferlen)
+{
+  QByteArray data;
+  data.duplicate(_pcbuffer, _nbufferlen);
+  
+//   kdDebug(2) << "WebPriceQuoteProcess::slotReceivedDataFromFilter(): " << QString(data) << endl;
+
+  m_string += QString(data);
+}
+
+void WebPriceQuoteProcess::slotProcessExited(KProcess*)
+{
+//   kdDebug(2) << "WebPriceQuoteProcess::slotProcessExited()" << endl;
+  
+  emit processExited(m_string);
+  m_string.truncate(0);
+}
+
+//
+// Unit test helpers
+//
+
+test::QuoteReceiver::QuoteReceiver(WebPriceQuote* q, QObject* parent, const char *name) :
+  QObject(parent,name)
+{
+  connect(q,SIGNAL(quote(const QString&,const QDate&, const MyMoneyMoney&)),
+    this,SLOT(slotGetQuote(const QString&,const QDate&, const MyMoneyMoney&)));
+  connect(q,SIGNAL(status(const QString&)),
+    this,SLOT(slotStatus(const QString&)));
+  connect(q,SIGNAL(error(const QString&)),
+    this,SLOT(slotError(const QString&)));
+}
+
+test::QuoteReceiver::~QuoteReceiver()
+{
+}
+
+void test::QuoteReceiver::slotGetQuote(const QString&,const QDate& d, const MyMoneyMoney& m)
+{
+//   kdDebug(2) << "test::QuoteReceiver::slotGetQuote( , " << d << " , " << m.toString() << " )" << endl;
+ 
+  m_price = m;
+  m_date = d;
+}
+void test::QuoteReceiver::slotStatus(const QString& msg)
+{
+//   kdDebug(2) << "test::QuoteReceiver::slotStatus( " << msg << " )" << endl;
+  
+  m_statuses += msg;
+}
+void test::QuoteReceiver::slotError(const QString& msg)
+{
+//   kdDebug(2) << "test::QuoteReceiver::slotError( " << msg << " )" << endl;
+  
+  m_errors += msg;
+}
+
+// vim:cin:si:ai:et:ts=2:sw=2:
---
+++ kmymoney2/converter/webpricequote.h
@@ -0,0 +1,169 @@
+/***************************************************************************
+                          webpricequote.h
+                             -------------------
+    begin                : Thu Dec 30 2004
+    copyright            : (C) 2004 by Ace Jones
+    email                : Ace Jones <acejones@users.sourceforge.net>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef WEBPRICEQUOTE_H 
+#define WEBPRICEQUOTE_H 
+
+// ----------------------------------------------------------------------------
+// QT Headers
+
+#include <qobject.h>
+#include <qdatetime.h>
+#include <qstringlist.h>
+#include <qmap.h>
+
+// ----------------------------------------------------------------------------
+// KDE Headers
+
+#include <kprocess.h>
+
+// ----------------------------------------------------------------------------
+// Project Headers
+
+#include "../mymoney/mymoneymoney.h"
+
+/**
+Helper class to attend the process which is running the script, in the case
+of a local script being used to fetch the quote.
+
+@author Thomas Baumgart <thb@net-bembel.de> & Ace Jones <acejones@users.sourceforge.net>
+*/
+class WebPriceQuoteProcess: public KProcess
+{
+  Q_OBJECT
+public:
+  WebPriceQuoteProcess(void);
+  void setSymbol(const QString& _symbol) { m_symbol = _symbol; m_string.truncate(0); }
+
+public slots:
+  void slotReceivedDataFromFilter(KProcess*, char*, int);
+  void slotProcessExited(KProcess*);
+
+signals:
+  void processExited(const QString&);
+
+private:
+  QString m_symbol;
+  QString m_string;
+};
+
+/**
+  * @author Thomas Baumgart & Ace Jones
+  *
+  * This is a helper class to store information about an online source
+  * for stock prices or currency exchange rates.
+  */
+struct WebPriceQuoteSource
+{
+  WebPriceQuoteSource() {};
+  WebPriceQuoteSource(const QString& name);
+  WebPriceQuoteSource(const QString& name, const QString& url, const QString& sym, const QString& price, const QString& date);
+  ~WebPriceQuoteSource() {};
+
+  void write(void) const;
+  void rename(const QString& name);
+  void remove(void) const;
+
+  QString    m_name;
+  QString    m_url;
+  QString    m_sym;
+  QString    m_price;
+  QString    m_date;
+};
+
+/**
+Retrieves a price quote from a web-based quote source
+
+@author Ace Jones <acejones@users.sourceforge.net>
+*/
+class WebPriceQuote: public QObject
+{
+  Q_OBJECT
+public:
+  WebPriceQuote( QObject* = 0, const char* = 0 );
+  ~WebPriceQuote();
+  
+  /**
+    * This launches a web-based quote update for the given @p _symbol.
+    * When the quote is received back from the web source, it will be
+    * emitted on the 'quote' signal.
+    *
+    * @param _symbol the trading symbol of the stock to fetch a price for
+    * @param _source the source of the quote (must be a valid value returned
+    *                by quoteSources().  Send QString() to use the default
+    *                source.
+    * @return bool Whether the quote fetch process was launched successfully
+    */
+  bool launch(const QString& _symbol, const QString& _source=QString());
+
+  /**
+    * This returns a list of the names of the quote sources
+    * currently defined.
+    *
+    * @return QStringList of quote source names
+    */
+  static QStringList quoteSources(void);
+    
+signals:
+  void quote(const QString&, const QDate&, const MyMoneyMoney&);
+  void status(const QString&);
+  void error(const QString&);
+  
+protected slots:
+  void slotParseQuote(const QString&);
+
+protected:
+  static QMap<QString,WebPriceQuoteSource> defaultQuoteSources(void);
+  
+private:
+  WebPriceQuoteProcess m_filter;
+  QString m_symbol;
+  QDate m_date;
+  MyMoneyMoney m_price;
+  WebPriceQuoteSource m_source;  
+};
+
+namespace test {
+
+/**
+Simple class to handle signals/slots for unit tests 
+
+@author Ace Jones <acejones@users.sourceforge.net>
+*/
+class QuoteReceiver : public QObject
+{
+Q_OBJECT
+public:
+    QuoteReceiver(WebPriceQuote* q, QObject *parent = 0, const char *name = 0);
+    ~QuoteReceiver();
+public slots:
+  void slotGetQuote(const QString&,const QDate&, const MyMoneyMoney&);
+  void slotStatus(const QString&);
+  void slotError(const QString&);
+public:
+  QStringList m_statuses;
+  QStringList m_errors;
+  MyMoneyMoney m_price;
+  QDate m_date;
+};
+
+} // end namespace test
+
+
+#endif // WEBPRICEQUOTE_H
+
+// vim:cin:si:ai:et:ts=2:sw=2:
