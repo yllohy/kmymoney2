010e2ae23eb6e0edf46f0efab06a800e1e8aa1b3
Ace Jones <acejones@users.sourceforge.net>
2005-03-16 05:41:43 +0000

    #ifdef KMM_DEBUG, print out the stripped content of the quote data

--- kmymoney2/converter/webpricequote.cpp
+++ kmymoney2/converter/webpricequote.cpp
@@ -186,7 +186,7 @@ void WebPriceQuote::slotParseQuote(const QString& _quotedata)
     // Extra white space
     quotedata = quotedata.simplifyWhiteSpace();      
 
-#if 0
+#ifdef KMM_DEBUG
     // Enable to get a look at the data coming back from the source after it's stripped
     QFile file("stripped.txt");
     if ( file.open( IO_WriteOnly ) )
