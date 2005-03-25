22d2db8ae04eaf42db0f4c8477d0ff0f252f1871
Thomas Baumgart <ipwizard@users.sourceforge.net>
2005-03-25 10:09:48 +0000

    Added header file to support 'make bcheck'

--- kmymoney2/mymoney/mymoneyreport.h
+++ kmymoney2/mymoney/mymoneyreport.h
@@ -28,6 +28,7 @@ class QDomDocument;
 
 // ----------------------------------------------------------------------------
 // Project Includes
+#include "../mymoney/mymoneyaccount.h"
 #include "mymoneytransactionfilter.h"
 
 /**
