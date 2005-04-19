c93d3602b1b5638958de75fd3d7b0b6e8ad650ae
Ace Jones <acejones@users.sourceforge.net>
2005-04-19 02:04:58 +0000

    Applied patch #1183190: Online quotes locale patch   Submitted by Andy Thaller <gandy@users.sourceforge.net>   * Fixes an error in online quotes on locales which use a different     comma and period placement scheme from the quote source.

--- kmymoney2/dialogs/kequitypriceupdatedlg.cpp
+++ kmymoney2/dialogs/kequitypriceupdatedlg.cpp
@@ -341,7 +341,7 @@ void KEquityPriceUpdateDlg::slotReceivedQuote(const QString& _symbol,const QDate
       if ( date > QDate::currentDate() )
         date = QDate::currentDate();
 
-      item->setText(PRICE_COL, QString::number(_price,'f',m_pricePrecision));
+      item->setText(PRICE_COL, KGlobal::locale()->formatMoney(_price,NULL,m_pricePrecision));
       item->setText(DATE_COL, date.toString(Qt::ISODate));
       logStatusMessage(i18n("Price for %1 updated").arg(_symbol));
     }
