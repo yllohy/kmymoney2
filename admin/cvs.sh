41ac8cc94e1ddf9e46cdbc45eb96115895db7d7e
Thomas Baumgart <ipwizard@users.sourceforge.net>
2006-01-31 20:07:13 +0000

    Updated tool detection logic to the latest version from the KDE repository

--- admin/cvs.sh
+++ admin/cvs.sh
@@ -629,8 +629,9 @@ if test -f Makefile.am.in; then
   rm -f $makefile_wo
 fi
 
-# Suck in the AUTOCONF detection code
-. $admindir/detect-autoconf.sh
+# Call script to find autoconf and freinds. Uses eval since the script outputs
+# sh-compatible code.
+eval `$admindir/detect-autoconf.pl`
 
 ###
 ### Main
