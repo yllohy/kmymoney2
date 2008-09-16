fa9400f3a7652e79061c20729d8b51ba5a584ddd
Alvaro Soliverez <asoliverez@users.sourceforge.net>
2008-09-16 01:05:07 +0000

    Reinitialize m_forecast in Home Page on change - coded a little better

--- kmymoney2/views/khomeview.cpp
+++ kmymoney2/views/khomeview.cpp
@@ -1616,8 +1616,7 @@ void KHomeView::doForecast()
   m_accountList.clear();
 
   //reinitialize the object
-  MyMoneyForecast forecast;
-  m_forecast = forecast;
+  m_forecast = MyMoneyForecast();
 
   //If forecastDays lower than accountsCycle, adjust to the first cycle
   if(m_forecast.accountsCycle() > m_forecast.forecastDays())
